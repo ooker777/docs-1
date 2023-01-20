a75455df720dc6adcf79177a0a3fc3c9f42f87e7
Mara <mara-li@outlook.fr>
2023-01-20 12:58:04 +0100

    fix post list & meta

--- docs/assets/js/utils.js
+++ docs/assets/js/utils.js
@@ -158,3 +158,14 @@ window.onload = function () {
         attributeFilter: ['data-md-color-scheme'],
     })
 }
+
+var paletteSwitcher1 = document.getElementById("__palette_1");
+var paletteSwitcher2 = document.getElementById("__palette_2");
+
+paletteSwitcher1.addEventListener("change", function () {
+    location.reload();
+});
+
+paletteSwitcher2.addEventListener("change", function () {
+    location.reload();
+});
