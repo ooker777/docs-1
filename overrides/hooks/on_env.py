df32cf82cc5c77ed917a17860c76d884066169c7
Lisandra-dev <mara-li@outlook.fr>
2023-05-25 16:41:58 +0000

    [SYNC] synced local 'overrides/' with remote 'overrides/'
    Updated from Mara-Li blog

--- overrides/hooks/on_env.py
+++ overrides/hooks/on_env.py
@@ -124,7 +124,6 @@ def time_to_iso(time):
     Returns:
         any|str: convert time or the original time if error
     """
-    
     if isinstance(time, datetime.time) or isinstance(time, datetime.date):
         time = time.isoformat()
     time = time.replace("-", "/")
